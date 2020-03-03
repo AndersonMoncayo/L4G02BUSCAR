4859f2ba4eac47f7804f214f7b147bcc22158442
Your Name <you@example.com>
2020-03-02 19:41:10 -0500

    anderson

---
+++ arraybuscar.php
@@ -0,0 +1,23 @@
+<?php
+$var= letras;
+$var= resultado;
+$var= palabrabuscar;
+$var= letrabuscar;
+$palabrabuscar = "hola";
+for (i=0; i<= palabrabuscar.lent; i++){
+$letrabuscar = substr(palabrabuscar,i,i);
+$ubicacionletras= letras.find(letras,letrabuscar);
+if($ubicacionletras>0){
+    $resultado = rusultado + letrabuscar;
+}
+}
+if($resultado==palabrabuscar);
+{
+    print("palabra encontrada");
+}
+else
+{
+    print("nose encuentra";)
+}
+}
+?>
